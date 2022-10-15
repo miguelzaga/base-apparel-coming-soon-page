3772b46f23c29d195f08f11a34e44f8399712c60
Miguel <miguezaga2@hotmail.com>
2022-10-15 09:15:04 -0500

    add email validation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import { useState } from "react";
 import logo from "/src/images/logo.svg";
 import iconArrow from "/src/images/icon-arrow.svg";
+import iconError from "/src/images/icon-error.svg";
 import heroMobile from "/src/images/hero-mobile.jpg";
 
 function App() {
+  var [error, setError] = useState(false);
+  var [input, setInput] = useState("");
+
+  function isValidEmail(email) {
+    const regex =
+      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return regex.test(email);
+  }
+
+  function handleSubmit(event) {
+    if (isValidEmail(input)) {
+      window.alert(`You have subscribed succesfully with the email: ${input}`);
+    } else {
+      console.log("error");
+      setError(true);
+    }
+    event.preventDefault();
+  }
+
   return (
     <div>
       <header className="header max-width">
@@ -21,9 +41,11 @@ function App() {
             store. Add your email below to stay up-to-date with announcements
             and our launch deals.
           </p>
-          <form className="main__form">
+          <form className="main__form" onSubmit={handleSubmit}>
             <input
               type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
               className="main__input"
               placeholder="Email Address"
             />
